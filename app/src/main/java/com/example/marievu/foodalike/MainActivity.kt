32f86c554809a610a0d7193cd61209da3d46bb75
marievu <marie.vu@mail.mcgill.ca>
2018-11-18 00:47:26 -0500

    feature camera added

--- app/src/main/java/com/example/marievu/foodalike/MainActivity.kt
+++ app/src/main/java/com/example/marievu/foodalike/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.marievu.foodalike
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener{
+            val intent= Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
