e5dbce5096912f05ced007ef218a1adf996ba1fe
marievu <marie.vu@mail.mcgill.ca>
2018-11-17 22:30:26 -0500

    first commit

---
+++ app/src/main/java/com/example/marievu/foodalike/CameraActivity.kt
@@ -0,0 +1,17 @@
+package com.example.marievu.foodalike
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_camera.*
+
+class CameraActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+
+
+
+
+}
